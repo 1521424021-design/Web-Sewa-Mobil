7b9297790df1e8055d918e96708067051866895c
1521424021-desain <1521424021@mahasiswa.ung.ac.id>
2026-05-25 01:35:47 +0700

    fix allowed hosts domain server

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-joyir-^eozll&(@idryrq6zru#y@&81xhgmv*r@jaqz@rp@v$q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Sewamobil.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
